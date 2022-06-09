70298ff438c5194684333dbcec464921ec5169f5
Futureth <Mrternine001@gmail>
2022-06-10 00:41:44 +0700

    upload new site Aigent

---
+++ js/script.js
@@ -0,0 +1,19 @@
+function signup() {
+    window.open("https://app.aigentcorporation.com/signup");
+}
+
+function discord() {
+    window.open("https://discord.gg/3xNa45WZfg");
+}
+
+function fb() {
+    window.open("https://www.facebook.com/Aigent.corp");
+}
+
+function tw() {
+    window.open("https://twitter.com/Aigent_corp");
+}
+
+function inst() {
+    window.open("https://www.instagram.com/aigentcorporation/");
+}
